c1dacd76edea1083914a2ec6ec458e9692fcdb42
James Schumann <jschumann@gmail.com>
2019-04-01 10:30:30 -0400

    feature flags; multiple functions; support scheduled posts

--- lamson-client.php
+++ lamson-client.php
@@ -11,235 +11,73 @@
 
 define('LAMSON_CLIENT_PRIORITY',12838790321);
 
-function Lamson_Client_init() {
-	Lamson_Client_register_hooks();
+function lamson_client_init() {
+	lamson_client_register_hooks();
 }
-add_action('init', 'Lamson_Client_init');
+add_action('init', 'lamson_client_init');
 
-function Lamson_Client_register_hooks() {
-	add_action('publish_post', 'Lamson_Client_publish_post_hook', LAMSON_CLIENT_PRIORITY, 2);
-	add_action('publish_page', 'Lamson_Client_publish_post_hook', LAMSON_CLIENT_PRIORITY, 2);
+function lamson_client_register_hooks() {
+	add_action('publish_post', 'lamson_client_publish_post_hook', LAMSON_CLIENT_PRIORITY, 2);
+	add_action('publish_page', 'lamson_client_publish_post_hook', LAMSON_CLIENT_PRIORITY, 2);
 
-	add_filter( 'rwmb_meta_boxes', 'wrdsb_lamson_post_edit_meta' );
+	add_filter( 'rwmb_meta_boxes', 'lamson_client_post_edit_meta' );
 }
 
-function wrdsb_lamson_post_edit_meta( $meta_boxes ) {
+function lamson_client_post_edit_meta( $meta_boxes ) {
 	$prefix = 'lamson_';
+	$lamson_features = (LAMSON_FEATURE_FLAGS) ? LAMSON_FEATURE_FLAGS : array();
+
+	$fields = array();
+	if ($lamson_features["email_notification_toggle"]) {
+		$fields[] = sendNotificationOptions($prefix);
+	}
+	if ($lamson_features["post_syndication_options"]) {
+		$fields[] = miscSyndicationOptions($prefix);
+		$fields[] = secondarySchoolOptions($prefix);
+		$fields[] = elementarySchoolOptions($prefix);
+	}
+	if ($lamson_features["test_syndication_options"]) {
+		$fields[] = testSyndicationOptions($prefix);
+	}
 
 	$meta_boxes[] = array(
 		'id' => 'notificationsandsyndication',
 		'title' => esc_html__( 'Notifications and Syndication', 'default' ),
-		'post_types' => array('post', 'page' ),
+		'post_types' => array('post'),
 		'context' => 'after_editor',
 		'priority' => 'default',
 		'autosave' => 'true',
-		'fields' => array(
-			array(
-				'id' => $prefix . 'send_notification',
-				'name' => esc_html__( 'Notify email subscribers?', 'default' ),
-				'type' => 'radio',
-				'desc' => esc_html__( 'When the "Publish" (or "Update") button is pressed, send a copy of this post to this site\'s email notification subscribers.', 'default' ),
-				'placeholder' => '',
-				'options' => array(
-					'true' => 'Yes',
-					'false' => 'No',
-				),
-				'inline' => 'true',
-				'std' => 'true',
-			),
-			array(
-				'id'      => $prefix . 'syndication_targets',
-				'name'    => 'Schools',
-				'type'    => 'checkbox_list',
-				// Options of checkboxes, in format 'value' => 'Label'
-				'options' => array(
-					'schools-all'        => 'All Schools',
-					'schools-elementary' => 'Elementary Schools',
-					'schools-secondary'  => 'Secondary Schools',
-				),
-			),
-			array(
-				'id'      => $prefix . 'syndication_targets',
-				'name'    => 'Secondary Schools',
-				'type'    => 'checkbox_list',
-				// Options of checkboxes, in format 'value' => 'Label'
-				'options' => array(
-					'schools-bci' => 'BCI',
-					'schools-chc' => 'CHCI',
-					'schools-eci' => 'ECI',
-					'schools-eds' => 'EDSS',
-					'schools-fhc' => 'FHCI',
-					'schools-gci' => 'GCI',
-					'schools-gps' => 'GPSS',
-					'schools-grc' => 'GRCI',
-					'schools-hrh' => 'HHSS',
-					'schools-jhs' => 'JHSS',
-					'schools-kci' => 'KCI',
-					'schools-phs' => 'PHS',
-					'schools-jam' => 'SJAM',
-					'schools-sss' => 'SSS',
-					'schools-wci' => 'WCI',
-					'schools-wod' => 'WODSS',
-				),
-				// Display options in a single row?
-				// 'inline' => true,
-				// Display "Select All / None" button?
-				'select_all_none' => true,
-			),
-			array(
-				'id'      => $prefix . 'syndication_targets',
-				'name'    => 'Elementary Schools',
-				'type'    => 'checkbox_list',
-				// Options of checkboxes, in format 'value' => 'Label'
-				'options' => array(
-					'schools-ark' => 'A R Kaufman',
-					'schools-abe' => 'Abraham Erb',
-					'schools-alp' => 'Alpine',
-					'schools-ave' => 'Avenue Road',
-					'schools-ayr' => 'Ayr',
-
-					'schools-bdn' => 'Baden',
-					'schools-blr' => 'Blair Road',
-					'schools-bre' => 'Breslau',
-					'schools-brp' => 'Bridgeport',
-					'schools-bgd' => 'Brigadoon',
-
-					'schools-cdc' => 'Cedar Creek',
-					'schools-ced' => 'Cedarbrae',
-					'schools-cnc' => 'Centennial (Cambridge)',
-					'schools-cnw' => 'Centennial (Waterloo)',
-					'schools-ctr' => 'Central',
-					'schools-cha' => 'Chalmers Street',
-					'schools-chi' => 'Chicopee Hills',
-					'schools-cle' => 'Clemens Mill',
-					'schools-con' => 'Conestogo',
-					'schools-cor' => 'Coronation',
-					'schools-coh' => 'Country Hills',
-					'schools-crl' => 'Courtland',
-					'schools-cre' => 'Crestview',
-
-					'schools-doo' => 'Doon',
-					'schools-dpk' => 'Driftwood Park',
-
-					'schools-est' => 'Edna Staebler',
-					'schools-elg' => 'Elgin Street',
-					'schools-elz' => 'Elizabeth Ziegler',
-					'schools-emp' => 'Empire',
-
-					'schools-flo' => 'Floradale',
-					'schools-fgl' => 'Forest Glen',
-					'schools-fhl' => 'Forest Hill',
-					'schools-fra' => 'Franklin',
-
-					'schools-gcp' => 'Glencairn',
-					'schools-gvc' => 'Grand View (Cambridge)',
-					'schools-gvn' => 'Grandview (New Hamburg)',
-					'schools-gro' => 'Groh',
-
-					'schools-hes' => 'Hespeler',
-					'schools-hig' => 'Highland',
-					'schools-hil' => 'Hillcrest',
-					'schools-how' => 'Howard Robertson',
-
-					'schools-jfc' => 'J F Carmichael',
-					'schools-jwg' => 'J W Gerth',
-					'schools-jme' => 'Janet Metcalfe',
-					'schools-jst' => 'Jean Steckle',
-					'schools-jdp' => 'John Darling',
-					'schools-jma' => 'John Mahood',
-
-					'schools-kea' => 'Keatsway',
-					'schools-ked' => 'King Edward',
-
-					'schools-lkw' => 'Lackner Woods',
-					'schools-lrw' => 'Laurelwood',
-					'schools-lau' => 'Laurentian',
-					'schools-lbp' => 'Lester B Pearson',
-					'schools-lex' => 'Lexington',
-					'schools-lnh' => 'Lincoln Heights',
-					'schools-lin' => 'Linwood',
-
-					'schools-mcg' => 'MacGregor',
-					'schools-mck' => 'Mackenzie King',
-					'schools-man' => 'Manchester',
-					'schools-mrg' => 'Margaret Avenue',
-					'schools-mjp' => 'Mary Johnston',
-					'schools-mea' => 'Meadowlane',
-					'schools-mil' => 'Millen Woods',
-					'schools-mof' => 'Moffat Creek',
-
-					'schools-nam' => 'N A MacEachern',
-					'schools-ndd' => 'New Dundee',
-					'schools-nlw' => 'Northlake Woods',
-
-					'schools-pkm' => 'Park Manor',
-					'schools-pkw' => 'Parkway',
-					'schools-pio' => 'Pioneer Park',
-					'schools-pre' => 'Preston',
-					'schools-pru' => 'Prueter',
-
-					'schools-qel' => 'Queen Elizabeth',
-					'schools-qsm' => 'Queensmount',
-
-					'schools-riv' => 'Riverside',
-					'schools-roc' => 'Rockway',
-					'schools-rmt' => 'Rosemount',
-					'schools-rye' => 'Ryerson',
-
-					'schools-sag' => 'Saginaw',
-					'schools-shl' => 'Sandhills',
-					'schools-snd' => 'Sandowne',
-					'schools-she' => 'Sheppard',
-					'schools-sil' => 'Silverheights',
-					'schools-sab' => 'Sir Adam Beck',
-					'schools-smi' => 'Smithson',
-					'schools-srg' => 'Southridge',
-					'schools-sta' => 'St Andrew\'s',
-					'schools-stj' => 'St Jacobs',
-					'schools-stn' => 'Stanley Park',
-					'schools-stw' => 'Stewart Avenue',
-					'schools-sud' => 'Suddaby',
-					'schools-sun' => 'Sunnyside',
-
-					'schools-tai' => 'Tait Street',
-					'schools-tri' => 'Trillium',
-
-					'schools-vis' => 'Vista Hills',
-
-					'schools-wtt' => 'W T Townshend',
-					'schools-wel' => 'Wellesley',
-					'schools-wsh' => 'Westheights',
-					'schools-wsm' => 'Westmount',
-					'schools-wsv' => 'Westvale',
-					'schools-wgd' => 'William G Davis',
-					'schools-wlm' => 'Williamsburg',
-					'schools-wls' => 'Wilson Avenue',
-					'schools-wcp' => 'Winston Churchill',
-					'schools-wpk' => 'Woodland Park',
-				),
-				// Display options in a single row?
-				// 'inline' => true,
-				// Display "Select All / None" button?
-				'select_all_none' => true,
-			),
-			array(
-				'id'      => $prefix . 'syndication_targets',
-				'name'    => 'Testing',
-				'type'    => 'checkbox_list',
-				// Options of checkboxes, in format 'value' => 'Label'
-				'options' => array(
-					'wplabs-didi'    => "Diana's lab",
-					'wplabs-becks'   => "Becky's lab",
-					'wplabs-cubicle' => "Jane's lab",
-				),
-			),
-		),
+		'fields' => $fields,
 	);
 
 	return $meta_boxes;
 }
 
-function Lamson_Client_publish_post_hook($ID, $post) {
+function lamson_client_publish_post_hook($ID, $post) {
+	$obj_to_post = buildLamsonWPPost($ID, $post);
+
+	$lamson_send_notification = get_post_meta($post->ID, 'lamson_send_notification', true);
+	if (! $lamson_send_notification) {
+		$lamson_send_notification = $_POST['lamson_send_notification'];
+	}
+	$obj_to_post['lamson_send_notification'] = $lamson_send_notification;
+
+	$syndication_targets = [];
+	$lamson_syndication_targets = get_post_meta($post->ID, 'lamson_syndication_targets', false);
+	if (! $lamson_syndication_targets) {
+		$lamson_syndication_targets = $_POST['lamson_syndication_targets'];
+	}
+	if ($lamson_syndication_targets) {
+		foreach ($lamson_syndication_targets as $target) {
+			$syndication_targets[] = $target;
+		}
+	}
+	$obj_to_post['lamson_syndication_targets'] = $syndication_targets;
+
+	$result = lamson_client_post_request($obj_to_post);
+}
+
+function buildLamsonWPPost($ID, $post) {
 	$site_details = get_blog_details(get_current_blog_id());
 
 	$site_url = str_replace('http://', '', site_url());
@@ -325,28 +163,22 @@ function Lamson_Client_publish_post_hook($ID, $post) {
 	}
 	$obj_to_post['post_tags'] = $post_tags;
 
-	$obj_to_post['lamson_send_notification'] = $_POST['lamson_send_notification'];
+	return $obj_to_post;
+}
 
-	$syndication_targets = [];
-	$targets = $_POST['lamson_syndication_targets'];
-	if ($targets) {
-		foreach ($targets as $target) {
-			$syndication_targets[] = $target;
-		}
-	}
-	$obj_to_post['lamson_syndication_targets'] = $syndication_targets;
-
-	$obj_to_post = json_encode($obj_to_post);
+function lamson_client_post_request($obj_to_post) {
+	$post_type = $obj_to_post['post_type'];
+	$encoded_obj = json_encode($obj_to_post);
 
 	$request = array(
 		'headers' => array(
 			'Content-Type' => 'application/json',
 			//'x-api-key' => LAMSON_API_KEY
 		),
-		'body' => $obj_to_post
+		'body' => $encoded_obj
 	);
 
-	switch ($post->post_type) {
+	switch ($post_type) {
 		case 'post':
 			return wp_remote_post(LAMSON_CLIENT_WP_POSTS_POST, $request);
 			break;
@@ -354,4 +186,218 @@ function Lamson_Client_publish_post_hook($ID, $post) {
 			return wp_remote_post(LAMSON_CLIENT_WP_PAGES_POST, $request);
 			break;
 	}
+}
+
+function sendNotificationOptions($prefix) {
+	return array(
+		'id' => $prefix . 'send_notification',
+		'name' => esc_html__( 'Notify email subscribers?', 'default' ),
+		'type' => 'radio',
+		'desc' => esc_html__( 'When the "Publish" (or "Update") button is pressed, send a copy of this post to this site\'s email notification subscribers.', 'default' ),
+		'placeholder' => '',
+		'options' => array(
+			'true' => 'Yes',
+			'false' => 'No',
+		),
+		'inline' => 'true',
+		'std' => 'true',
+	);
+}
+
+function miscSyndicationOptions($prefix) {
+	return array(
+		'id'      => $prefix . 'syndication_targets',
+		'name'    => 'Schools',
+		'type'    => 'checkbox_list',
+		// Options of checkboxes, in format 'value' => 'Label'
+		'options' => array(
+			'schools-all'        => 'All Schools',
+			'schools-elementary' => 'Elementary Schools',
+			'schools-secondary'  => 'Secondary Schools',
+		),
+	);
+}
+
+function testSyndicationOptions($prefix) {
+	return array(
+		'id'      => $prefix . 'syndication_targets',
+		'name'    => 'Testing',
+		'type'    => 'checkbox_list',
+		// Options of checkboxes, in format 'value' => 'Label'
+		'options' => array(
+			'wplabs-didi'    => "Diana's lab",
+			'wplabs-becks'   => "Becky's lab",
+			'wplabs-cubicle' => "Jane's lab",
+		),
+	);
+}
+
+function secondarySchoolOptions($prefix) {
+	return array(
+		'id'      => $prefix . 'syndication_targets',
+		'name'    => 'Secondary Schools',
+		'type'    => 'checkbox_list',
+		// Options of checkboxes, in format 'value' => 'Label'
+		'options' => array(
+			'schools-bci' => 'BCI',
+			'schools-chc' => 'CHCI',
+			'schools-eci' => 'ECI',
+			'schools-eds' => 'EDSS',
+			'schools-fhc' => 'FHCI',
+			'schools-gci' => 'GCI',
+			'schools-gps' => 'GPSS',
+			'schools-grc' => 'GRCI',
+			'schools-hrh' => 'HHSS',
+			'schools-jhs' => 'JHSS',
+			'schools-kci' => 'KCI',
+			'schools-phs' => 'PHS',
+			'schools-jam' => 'SJAM',
+			'schools-sss' => 'SSS',
+			'schools-wci' => 'WCI',
+			'schools-wod' => 'WODSS',
+		),
+		// Display options in a single row?
+		// 'inline' => true,
+		// Display "Select All / None" button?
+		'select_all_none' => true,
+	);
+}
+
+function elementarySchoolOptions($prefix) {
+	return array(
+		'id'      => $prefix . 'syndication_targets',
+		'name'    => 'Elementary Schools',
+		'type'    => 'checkbox_list',
+		// Options of checkboxes, in format 'value' => 'Label'
+		'options' => array(
+			'schools-ark' => 'A R Kaufman',
+			'schools-abe' => 'Abraham Erb',
+			'schools-alp' => 'Alpine',
+			'schools-ave' => 'Avenue Road',
+			'schools-ayr' => 'Ayr',
+
+			'schools-bdn' => 'Baden',
+			'schools-blr' => 'Blair Road',
+			'schools-bre' => 'Breslau',
+			'schools-brp' => 'Bridgeport',
+			'schools-bgd' => 'Brigadoon',
+
+			'schools-cdc' => 'Cedar Creek',
+			'schools-ced' => 'Cedarbrae',
+			'schools-cnc' => 'Centennial (Cambridge)',
+			'schools-cnw' => 'Centennial (Waterloo)',
+			'schools-ctr' => 'Central',
+			'schools-cha' => 'Chalmers Street',
+			'schools-chi' => 'Chicopee Hills',
+			'schools-cle' => 'Clemens Mill',
+			'schools-con' => 'Conestogo',
+			'schools-cor' => 'Coronation',
+			'schools-coh' => 'Country Hills',
+			'schools-crl' => 'Courtland',
+			'schools-cre' => 'Crestview',
+
+			'schools-doo' => 'Doon',
+			'schools-dpk' => 'Driftwood Park',
+
+			'schools-est' => 'Edna Staebler',
+			'schools-elg' => 'Elgin Street',
+			'schools-elz' => 'Elizabeth Ziegler',
+			'schools-emp' => 'Empire',
+
+			'schools-flo' => 'Floradale',
+			'schools-fgl' => 'Forest Glen',
+			'schools-fhl' => 'Forest Hill',
+			'schools-fra' => 'Franklin',
+
+			'schools-gcp' => 'Glencairn',
+			'schools-gvc' => 'Grand View (Cambridge)',
+			'schools-gvn' => 'Grandview (New Hamburg)',
+			'schools-gro' => 'Groh',
+
+			'schools-hes' => 'Hespeler',
+			'schools-hig' => 'Highland',
+			'schools-hil' => 'Hillcrest',
+			'schools-how' => 'Howard Robertson',
+
+			'schools-jfc' => 'J F Carmichael',
+			'schools-jwg' => 'J W Gerth',
+			'schools-jme' => 'Janet Metcalfe',
+			'schools-jst' => 'Jean Steckle',
+			'schools-jdp' => 'John Darling',
+			'schools-jma' => 'John Mahood',
+
+			'schools-kea' => 'Keatsway',
+			'schools-ked' => 'King Edward',
+
+			'schools-lkw' => 'Lackner Woods',
+			'schools-lrw' => 'Laurelwood',
+			'schools-lau' => 'Laurentian',
+			'schools-lbp' => 'Lester B Pearson',
+			'schools-lex' => 'Lexington',
+			'schools-lnh' => 'Lincoln Heights',
+			'schools-lin' => 'Linwood',
+
+			'schools-mcg' => 'MacGregor',
+			'schools-mck' => 'Mackenzie King',
+			'schools-man' => 'Manchester',
+			'schools-mrg' => 'Margaret Avenue',
+			'schools-mjp' => 'Mary Johnston',
+			'schools-mea' => 'Meadowlane',
+			'schools-mil' => 'Millen Woods',
+			'schools-mof' => 'Moffat Creek',
+
+			'schools-nam' => 'N A MacEachern',
+			'schools-ndd' => 'New Dundee',
+			'schools-nlw' => 'Northlake Woods',
+
+			'schools-pkm' => 'Park Manor',
+			'schools-pkw' => 'Parkway',
+			'schools-pio' => 'Pioneer Park',
+			'schools-pre' => 'Preston',
+			'schools-pru' => 'Prueter',
+
+			'schools-qel' => 'Queen Elizabeth',
+			'schools-qsm' => 'Queensmount',
+
+			'schools-riv' => 'Riverside',
+			'schools-roc' => 'Rockway',
+			'schools-rmt' => 'Rosemount',
+			'schools-rye' => 'Ryerson',
+
+			'schools-sag' => 'Saginaw',
+			'schools-shl' => 'Sandhills',
+			'schools-snd' => 'Sandowne',
+			'schools-she' => 'Sheppard',
+			'schools-sil' => 'Silverheights',
+			'schools-sab' => 'Sir Adam Beck',
+			'schools-smi' => 'Smithson',
+			'schools-srg' => 'Southridge',
+			'schools-sta' => 'St Andrew\'s',
+			'schools-stj' => 'St Jacobs',
+			'schools-stn' => 'Stanley Park',
+			'schools-stw' => 'Stewart Avenue',
+			'schools-sud' => 'Suddaby',
+			'schools-sun' => 'Sunnyside',
+
+			'schools-tai' => 'Tait Street',
+			'schools-tri' => 'Trillium',
+
+			'schools-vis' => 'Vista Hills',
+
+			'schools-wtt' => 'W T Townshend',
+			'schools-wel' => 'Wellesley',
+			'schools-wsh' => 'Westheights',
+			'schools-wsm' => 'Westmount',
+			'schools-wsv' => 'Westvale',
+			'schools-wgd' => 'William G Davis',
+			'schools-wlm' => 'Williamsburg',
+			'schools-wls' => 'Wilson Avenue',
+			'schools-wcp' => 'Winston Churchill',
+			'schools-wpk' => 'Woodland Park',
+		),
+		// Display options in a single row?
+		// 'inline' => true,
+		// Display "Select All / None" button?
+		'select_all_none' => true,
+	);
 }
