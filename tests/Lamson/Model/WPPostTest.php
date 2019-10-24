3fca386011ba1aea9c339e6d4107a81dcafc88d9
James Schumann <jschumann@gmail.com>
2019-10-24 14:41:55 -0400

    phpunit init

---
+++ tests/Lamson/Model/WPPostTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Lamson;
+
+use Lamson\Model\WPPost;
+use PHPUnit\Framework\TestCase;
+
+final class WPPostTest extends TestCase
+{
+    public function testCanBeInstantiated(): void
+    {
+        $this->assertTrue(true);
+    }
+}
