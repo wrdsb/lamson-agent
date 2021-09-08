f915ce3b35e7ae0d3d3b8d04d44b222abfb11b85
James Schumann <james@schumann.ca>
2021-09-08 10:54:30 -0400

    add all post meta to outgoing post object

--- lamson-client.php
+++ lamson-client.php
@@ -125,55 +125,66 @@ function buildLamsonWPPost($ID, $post) {
     $post_author_name = $author_details->first_name.' '.$author_details->last_name;
     $post_author_email = $author_details->user_email;
 
-    $obj_to_post = [
-        'id' => $site_domain.'_'.$site_slug.'_'.$post->ID,
+    $obj_to_post = [];
 
-        'post_id' => $post->ID,
-        'post_slug' => $post->post_name,
-        'post_guid' => $post->guid,
+    // Start with meta so it can be overwritten as needed
+    $allMeta = get_post_meta($post->ID);
+    foreach($allMeta as $key => $val) {
+        if (count($val) == 1) {
+            $obj_to_post[$key] = $val[0];
+        } else {
+            $obj_to_post[$key] = json_encode($val);
+        }
+    }
 
-        'post_type' => $post->post_type,
+    $obj_to_post['id'] = $site_domain.'_'.$site_slug.'_'.$post->ID;
 
-        'post_status' => $post->post_status,
+    $obj_to_post['post_id'] = $post->ID;
+    $obj_to_post['post_slug'] = $post->post_name;
+    $obj_to_post['post_guid'] = $post->guid;
 
-        // Probably not a good idea to include this field:
-        //'post_password' => $post->post_password,
+    $obj_to_post['post_type'] = $post->post_type;
 
-        'post_date' => $post->post_date,
-        'post_date_gmt' => $post->post_date_gmt,
-    
-        'post_modified' => $post->post_modified,
-        'post_modified_gmt' => $post->post_modified_gmt,
+    $obj_to_post['post_status'] = $post->post_status;
 
-        'post_author_id' => $post->post_author,
-        'post_author_name' => $post_author_name,
-        'post_author_email' => $post_author_email,
-    
-        'post_title' => $post->post_title,
-        'post_content' => $post->post_content,
-        'post_excerpt' => $post->post_excerpt,
+    // Probably not a good idea to include this field, so we null it
+    $obj_to_post['post_password'] = null;
 
-        'post_parent' => $post->post_parent,
-        'post_menu_order' => $post->menu_order,
+    $obj_to_post['post_date'] = $post->post_date;
+    $obj_to_post['post_date_gmt'] = $post->post_date_gmt;
 
-        'comment_status' => $post->comment_status,
-        'comment_count' => $post->comment_count,
-    
-        'ping_status' => $post->ping_status,
-        'pinged' => $post->pinged,
-        'to_ping' => $post->to_ping,
-    
-        'post_filter' => $post->filter,
-        'post_content_filtered' => $post->post_content_filtered,
-        'post_mime_type' => $post->post_mime_type,
+    $obj_to_post['post_modified'] = $post->post_modified;
+    $obj_to_post['post_modified_gmt'] = $post->post_modified_gmt;
 
-        'site_url' => $site_url,
-        'site_domain' => $site_domain,
-        'site_slug' => $site_slug,
-        'site_name' => $site_name,
-        'site_link' => $site_link,
-        'site_privacy' => $site_privacy,
-    ];
+    $obj_to_post['post_author_id'] = $post->post_author;
+    $obj_to_post['post_author_name'] = $post_author_name;
+    $obj_to_post['post_author_email'] = $post_author_email;
+
+    $obj_to_post['post_title'] = $post->post_title;
+    $obj_to_post['post_content'] = $post->post_content;
+    $obj_to_post['post_excerpt'] = $post->post_excerpt;
+
+    $obj_to_post['post_parent'] = $post->post_parent;
+    $obj_to_post['post_menu_order'] = $post->menu_order;
+
+    $obj_to_post['comment_status'] = $post->comment_status;
+    $obj_to_post['comment_count'] = $post->comment_count;
+
+    $obj_to_post['ping_status'] = $post->ping_status;
+    $obj_to_post['pinged'] = $post->pinged;
+    $obj_to_post['to_ping'] = $post->to_ping;
+
+    $obj_to_post['post_filter'] = $post->filter;
+    $obj_to_post['post_content_filtered'] = $post->post_content_filtered;
+    $obj_to_post['post_mime_type'] = $post->post_mime_type;
+
+    $obj_to_post['site_url'] = $site_url;
+    $obj_to_post['site_domain'] = $site_domain;
+    $obj_to_post['site_slug'] = $site_slug;
+    $obj_to_post['site_name'] = $site_name;
+    $obj_to_post['site_link'] = $site_link;
+    $obj_to_post['site_privacy'] = $site_privacy;
+
 
     $post_categories = [];
     $categories = get_the_terms($post->ID, 'category');
@@ -184,6 +195,7 @@ function buildLamsonWPPost($ID, $post) {
     }
     $obj_to_post['post_categories'] = $post_categories;
 
+
     $post_tags = [];
     $tags = get_the_terms($post->ID, 'post_tag');
     if ($tags) {
@@ -192,6 +204,7 @@ function buildLamsonWPPost($ID, $post) {
         }
     }
     $obj_to_post['post_tags'] = $post_tags;
+
 
     $visible_to = [];
     switch ($obj_to_post->site_privacy) {
